91ceaea9d6ca2206b9c5f5e611799e6f730b8e59
Jason <cognitiaclaeves@users.noreply.github.com>
2024-05-07 13:27:50 -0400

    update backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,33 @@ resource "null_resource" "example" {
     value = "A example resource that does nothing!"
     }
 }
+
+terraform {
+  backend "s3" {
+    bucket = "tf-state-jnorment"
+    key    = "global/savvy-example/terraform.tfstate"
+    region = "us-east-1"
+
+    # dynamodb_table = "rearc-data-terraform-state-locks"
+    encrypt        = true
+  }
+}
+
+#     terraform {
+#       backend "remote" {
+#         # The name of your Terraform Cloud organization.
+#         organization = "example-organization"
+#
+#         # The name of the Terraform Cloud workspace to store Terraform state files in.
+#         workspaces {
+#           name = "example-workspace"
+#         }
+#       }
+#     }
+#
+#     # An example resource that does nothing.
+#     resource "null_resource" "example" {
+#       triggers = {
+#         value = "A example resource that does nothing!"
+#       }
+#     }
